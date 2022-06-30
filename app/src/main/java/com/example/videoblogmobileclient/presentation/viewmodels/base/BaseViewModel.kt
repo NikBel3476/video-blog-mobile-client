a8d5723de8cdad1dabb2c10b8be5f97578a0c946
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-06-30 16:06:38 +0300

    feat: add register retrofit request

--- app/src/main/java/com/example/videoblogmobileclient/presentation/viewmodels/base/BaseViewModel.kt
+++ app/src/main/java/com/example/videoblogmobileclient/presentation/viewmodels/base/BaseViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.videoblogmobileclient.presentation.viewmodels.base
 
-class BaseViewModel {
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+class BaseViewModel: ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+
+    fun Disposable.untilCleared(): Disposable {
+        addDisposable(this)
+        return this
+    }
+
+    private fun addDisposable(disposable: Disposable) {
+        compositeDisposable.add(disposable)
+    }
 }
