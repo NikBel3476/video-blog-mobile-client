ee506101b726c53fda190dea4dabbbf9ff8abb60
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-07-01 14:41:42 +0300

    feat: add navigation

---
+++ app/src/main/java/com/example/videoblogmobileclient/presentation/fragments/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.example.videoblogmobileclient.presentation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.videoblogmobileclient.R
+import com.example.videoblogmobileclient.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/videoblogmobileclient/presentation/fragments/WrapperFragment.kt
@@ -0,0 +1,31 @@
+package com.example.videoblogmobileclient.presentation.fragments
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.videoblogmobileclient.R
+import com.example.videoblogmobileclient.databinding.FragmentWrapperBinding
+import com.example.videoblogmobileclient.utils.Constants
+
+class WrapperFragment : Fragment() {
+    lateinit var binding: FragmentWrapperBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentWrapperBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_wrapperFragment_to_signUpSingInFragment)
+        }, Constants.WRAPPER_TIME)
+    }
+}
