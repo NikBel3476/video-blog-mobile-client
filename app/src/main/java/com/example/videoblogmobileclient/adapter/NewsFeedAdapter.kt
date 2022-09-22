e31aa87893d147beabf8b64d4e2833e19a10a632
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-09-22 16:07:02 +0300

    feat: add recycler view adapter and holder

--- app/src/main/java/com/example/videoblogmobileclient/adapter/NewsFeedAdapter.kt
+++ app/src/main/java/com/example/videoblogmobileclient/adapter/NewsFeedAdapter.kt
@@ -1,25 +1,51 @@
 package com.example.videoblogmobileclient.adapter
 
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.videoblogmobileclient.data.models.VideotapeContent
+import com.example.videoblogmobileclient.databinding.VideoContentItemBinding
 
 class NewsFeedAdapter: RecyclerView.Adapter<NewsFeedAdapter.NewsFeedHolder>() {
 
+    private var news = mutableListOf<VideotapeContent>()
 
     class NewsFeedHolder(item: View): RecyclerView.ViewHolder(item) {
+        private val binding = VideoContentItemBinding.bind(item)
 
+        fun bind(content: VideotapeContent) {
+            binding.tvTextContent.text = content.text_content
+            binding.authorName.text = content.author.login
+
+            // TODO: Add Glide to set author icon
+
+            // TODO: Add media player to show video
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsFeedHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = VideoContentItemBinding.inflate(inflater, parent, false)
+
+        return NewsFeedHolder(binding.root)
     }
 
     override fun onBindViewHolder(holder: NewsFeedHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = news[position]
+        holder.bind(item)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return news.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun fillNewsList(list: MutableList<VideotapeContent>?) {
+        if(!list.isNullOrEmpty())
+            news = list
+        else return
+        notifyDataSetChanged()
     }
 }
